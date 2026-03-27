a7579ba77cc9cb00420f358b136ca753c798c0ab
Renan <renanhoruz@gmail.com>
2026-03-27 09:45:51 +0200

    interfaces refactor: Consulted AI on how to utilize only one List

--- mod-3/src/main/java/com/mod3/app/App.java
+++ mod-3/src/main/java/com/mod3/app/App.java
@@ -9,14 +9,13 @@ public class App {
 
   /// NOTE: This is 3.2 task 1
   private static void one() {
-    List<IEVehicle> eVehicles = new ArrayList<>();
     List<IVehicle> vehicles = new ArrayList<>();
     vehicles.add(new Car());
     vehicles.add(new Bus());
     vehicles.add(new Motorcycle());
-    eVehicles.add(new EMotorcycle());
-    eVehicles.add(new ECar());
+    vehicles.add(new EMotorcycle());
+    vehicles.add(new ECar());
     VehicleDemo demo = new VehicleDemo();
-    demo.getInfo(vehicles, eVehicles);
+    demo.getInfo(vehicles);
   }
 }
--- mod-3/src/main/java/com/mod3/app/VahicleDemo.java
+++ mod-3/src/main/java/com/mod3/app/VahicleDemo.java
@@ -4,17 +4,15 @@ import java.util.*;
 
 class VehicleDemo {
 
-  public void getInfo(List<IVehicle> vehicles, List<IEVehicle> eVehicles) {
+  public void getInfo(List<IVehicle> vehicles) {
     for (IVehicle vehicle : vehicles) {
       vehicle.start();
       vehicle.stop();
       vehicle.getInfo();
       vehicle.calcEfficiency();
-    }
-    for (IEVehicle vehicle : eVehicles) { // NOTE: Could not figure out how to do this in one array. Will check with AI
-                                          // if it is possible.
-      vehicle.charge();
-      vehicle.calcEfficiency();
+      if (vehicle instanceof IEVehicle eVehicle) {
+        eVehicle.charge();
+      }
     }
   }
 }
