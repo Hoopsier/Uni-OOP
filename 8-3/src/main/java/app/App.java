7d4083af93f2260ae605fb9bab437fc87bfba309
Hoopsy <renanhoruz@gmail.com>
2026-04-29 12:34:16 +0300

    8-3 task 2

--- 8-3/src/main/java/app/App.java
+++ 8-3/src/main/java/app/App.java
@@ -11,6 +11,50 @@ import java.util.Random;
  */
 public class App {
   public static void main(String[] args) {
+    // one();
+    two();
+  }
+
+  private static void two() {
+    List<Integer> numbers = new ArrayList<>();
+    Random rnd = new Random();
+    DoublingService doublingService = nums -> {
+      for (int i = 0; i < nums.size(); i++) {
+        nums.set(i, nums.get(i) * 2);
+      }
+    };
+    SummingSercive summingSercive = nums -> {
+      int sum = 0;
+      for (int num : nums) {
+        sum += num;
+      }
+      return sum;
+    };
+    for (int i = 0; i < 117; i++) {
+      numbers.add(rnd.nextInt(0, 118));
+    }
+    numbers.removeIf((number) -> number % 2 != 0);
+
+    doublingService.doubleAll(numbers);
+
+    System.out.println(summingSercive.getSum(numbers));
+
+    // for (Integer integer : numbers) {
+    // System.out.println(integer);
+    // }
+  }
+
+  @FunctionalInterface
+  interface DoublingService {
+    void doubleAll(List<Integer> nums);
+  }
+
+  @FunctionalInterface
+  interface SummingSercive {
+    int getSum(List<Integer> nums);
+  }
+
+  private static void one() {
     List<Person> people = new ArrayList<>();
     Comparator<Person> personComparator = (person1, person2) -> {
       return person2.getAge() - person1.getAge();
